8393ac084cfc84bb317cc2c4bf6f1d1268cc0ff0
Aline-G <65853969+Aline-G@users.noreply.github.com>
2022-01-16 14:15:29 +0100

    fill the line table doesn't compile !!

--- src/app/home-screen/home-expenses/home-expenses.component.ts
+++ src/app/home-screen/home-expenses/home-expenses.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {LineBill} from "../../model/lineBill";
+import {ApiService} from "../../services/api.service";
 
 @Component({
   selector: 'app-home-expenses',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-expenses.component.css']
 })
 export class HomeExpensesComponent implements OnInit {
-
-  constructor() { }
+  lineBills!: LineBill[];
+  constructor(private apiService:ApiService) { }
 
   ngOnInit(): void {
+    this.apiService.getLineBillList()
+      .subscribe({
+        next: (res) => {
+          this.lineBills = res;
+          console.log(res);
+        },
+        error: (e) => console.error(e)
+      });
   }
 
 }
