016e7c70bb8947924ae43e6467bd7ef094fc761d
guillaume-besse <38402818+guillaume-besse@users.noreply.github.com>
2022-01-21 19:46:04 +0100

    ajout de homme-view-component-bills

---
+++ src/app/home-view-expense-bills/home-view-expense-bills.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {ExpenseBill} from "../model/expenseBill";
+import {ApiService} from "../services/api.service";
+import {SharedService} from "../services/dynamical-functions/SharedService";
+
+@Component({
+  selector: 'app-home-view-expense-bills',
+  templateUrl: './home-view-expense-bills.component.html',
+  styleUrls: ['./home-view-expense-bills.component.css']
+})
+export class HomeViewExpenseBillsComponent implements OnInit {
+
+  expenseBills!: ExpenseBill[];
+
+  constructor(private apiService: ApiService, private sharedService : SharedService) { }
+
+  ngOnInit(): void {
+    this.apiService.getExpenseBillList()
+      .subscribe({
+        next: (res) => {
+          this.expenseBills = res;
+          console.log(res);
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  onBillClicked(id : number) : void {
+    //this.sharedService.clickOnBillEvent.emit(id);
+  }
+}
