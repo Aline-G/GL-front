bc761a716cacee5ed8c1052d6f5a607cb2544626
atn99 <neyraud.antoan@gmail.com>
2022-01-24 17:20:16 +0100

    Validation component
    Début

---
+++ src/app/validation/validation-navbar/validation-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-validation-navbar',
+  templateUrl: './validation-navbar.component.html',
+  styleUrls: ['./validation-navbar.component.css']
+})
+export class ValidationNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
