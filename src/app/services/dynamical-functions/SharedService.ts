9bf3b7927f369068a73c95b2946c860984d48e53
RoibLS <92245844+RoibLS@users.noreply.github.com>
2022-01-28 18:24:30 +0100

    Try to add a button to bill tab which creates a new line for a bill
    ATTENTION  :anger:
    - error at creating a new linebill
    - error at suppress a note
    -  'amount'  for 'FRAIS KILO' not finished

--- src/app/services/dynamical-functions/SharedService.ts
+++ src/app/services/dynamical-functions/SharedService.ts
@@ -7,11 +7,13 @@ export class SharedService {
   clickOnBillEvent: EventEmitter<number> = new EventEmitter();
   clickOnAddBill: EventEmitter<boolean> = new EventEmitter();
   clickOnAlert: EventEmitter<string[]> = new EventEmitter();
+  clickOnPlusEvent: EventEmitter<number> = new EventEmitter();
 
   /*this function allows to delete a bill in confirmation delete component,
    the first argument is the id of the bill to delete and the second one is 0 which is a code to say that it's a bill*/
   billDelete: EventEmitter<number[]> = new EventEmitter();
   lineBillDelete: EventEmitter<number[]> = new EventEmitter();
   advanceDelete: EventEmitter<number[]> = new EventEmitter();
+
 
 }
