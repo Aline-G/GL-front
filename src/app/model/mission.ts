5f5818e10ffceff2d1b9d4b86b0e7d6aafc2e859
RoibLS <92245844+RoibLS@users.noreply.github.com>
2022-01-29 15:13:38 +0100

    beginning of mission management

--- src/app/model/mission.ts
+++ src/app/model/mission.ts
@@ -1,14 +1,36 @@
+enum missionStates {
+  IN_PROGRESS = "IN_PROGRESS",
+  FINISHED = "FINISHED",
+  SUSPENDED = "SUSPENDED",
+}
+
 export class Mission {
+
+
   id: number;
   name: string;
+  dateBeginning: Date;
+  dateEnding: Date;
+  missionState: missionStates;
+  referent:string; //Users
+  asssociated_collaborators :string; //Users list
   description: string;
-  date: Date;
 
 
-  constructor(id: number, name: string, description: string, date: Date) {
+
+  constructor(id: number, name: string, dateBeginning: Date, dateEnding: Date, missionState : missionStates, referent : string, associated_collaborators :string, description: string) {
     this.id=id;
     this.name=name;
+    this.dateBeginning=dateBeginning;
+    this.dateEnding=dateEnding;
+    this.missionState =missionState;
+    this.referent=referent;
+    this.asssociated_collaborators=associated_collaborators;
     this.description=description;
-    this.date=date;
+
+
+
+
+
   }
 }
